ae085faf8a76326b897e27f55536a7d3093ab9c1
Oliver Marketos <olivermarketos@gmail.com>
2024-09-15 14:02:22 +0200

    x axis shows actual time intervals

--- plots.py
+++ plots.py
@@ -3,6 +3,70 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+def test_multi_panel_gene_expression(df):
+    
+    figures = []
+    grouped = df.groupby(['gene_name', 'treatment'])
+
+    # Iterate over the groups and plot each
+    for (gene, treatment), group in grouped:
+        # Create a new figure for each gene and treatment
+        # fig, ax = plt.subplots(figsize=(8, 6))
+        fig, (ax, ax2) = plt.subplots(1, 2, sharey=True, facecolor='w')
+
+
+        # Plot points for individual replicates
+        ax.scatter(group['treatment_time'], group['log2_expression'], label='Replicates', color='blue', alpha=0.6)
+        ax2.scatter(group['treatment_time'], group['log2_expression'], label='Replicates', color='blue', alpha=0.6)
+        
+        # Calculate the mean log2_expression for each treatment_time
+        avg_group = group.groupby('treatment_time').agg({'log2_expression': 'mean'}).reset_index()
+
+        # Plot the average line
+        ax.plot(avg_group['treatment_time'], avg_group['log2_expression'], label=f"{gene} ({treatment}) Avg", color='black', marker='o')
+        ax2.plot(avg_group['treatment_time'], avg_group['log2_expression'], label=f"{gene} ({treatment}) Avg", color='black', marker='o')
+
+        ax.set_xlim(0, 30)
+        ax2.set_xlim(45, 50)
+
+        ax.spines['right'].set_visible(False)
+        ax2.spines['left'].set_visible(False)
+        ax.yaxis.tick_left()
+        ax.tick_params(labelright='off')
+        ax2.yaxis.tick_right()
+        
+        d = .015  # how big to make the diagonal lines in axes coordinates
+        # arguments to pass plot, just so we don't keep repeating them
+        kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
+        ax.plot((1-d, 1+d), (-d, +d), **kwargs)
+        #ax.plot((1-d, 1+d), (1-d, 1+d), **kwargs)
+
+        kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
+        ax2.plot((-d, +d), (-d, +d), **kwargs)
+       
+        # Add labels and title
+        ax.set_xlabel('Treatment Time')
+        ax.set_ylabel('Log2 Expression')
+        ax.set_title(f"Gene: {gene} | {treatment}hydration   ")
+        ax.legend()
+
+        figures.append(fig)
+   
+    return figures
+
+
+   
+
+    
+    # hide the spines between ax and ax2
+   
+    # This looks pretty good, and was fairly painless, but you can get that
+    # cut-out diagonal lines look with just a bit more work. The important
+    # thing to know here is that in axes coordinates, which are always
+    # between 0-1, spine endpoints are at these locations (0, 0), (0, 1),
+    # (1, 0), and (1, 1).  Thus, we just need to put the diagonals in the
+    # appropriate corners of each of our axes, and so long as we use the
+    # right transform and disable clipping.
 
 def multi_panel_gene_expression(df):
    
@@ -23,6 +87,7 @@ def multi_panel_gene_expression(df):
         # Plot the average line
         ax.plot(avg_group['treatment_time'], avg_group['log2_expression'], label=f"{gene} ({treatment}) Avg", color='black', marker='o')
 
+        ax.set_xticks(group['treatment_time'])
         # Add labels and title
         ax.set_xlabel('Treatment Time')
         ax.set_ylabel('Log2 Expression')
@@ -54,6 +119,9 @@ def single_panel_gene_expression(df):
 
             # Plot the average line for this gene
             ax.plot(avg_gene_group['treatment_time'], avg_gene_group['log2_expression'], label=f"{gene} Avg", marker='o')
+       
+       
+        ax.set_xticks(group['treatment_time'])
 
         # Add labels and title
         ax.set_xlabel('Treatment Time')
