b9901d8087adf215b20370a2d9e4d905762da8a6
Oliver Marketos <olivermarketos@gmail.com>
2024-12-23 11:50:13 +0100

    added search features to gene query page

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import streamlit as st
 # entry point app 
 home_page = st.Page("home.py", title="Home") #icon=":material/add_circle:")
 expression_page = st.Page("expression_page.py", title="Expression data",)
-gene_query_page = st.Page("gene_query_page.py", title="Gene info",)
-test_page = st.Page("test_page.py", title="Test page",)
-pg = st.navigation([home_page,expression_page,gene_query_page, test_page])
+
+
+# swap out the gene_query_page for the test_page TODO swap the test_page for the gene_query_page at some point
+gene_query_page= st.Page("gene_query_page.py", title="Gene info",)
+pg = st.navigation([home_page,expression_page,gene_query_page])
 st.set_page_config(page_title="Data explorer",page_icon=":material/edit:",layout="wide")
 
 pg.run()
