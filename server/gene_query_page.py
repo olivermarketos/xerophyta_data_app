4b517355a19485ffe41b2445eefc60561e89759d
Oliver Marketos <olivermarketos@gmail.com>
2025-02-26 16:25:15 +0100

    moved frontend query retreival logic to helper functions

--- server/gene_query_page.py
+++ server/gene_query_page.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime 
 import database.db as db  # Your custom db module
 from utils.constants import GENE_SELECTION_OPTIONS
-from utils.helper_functions import parse_input
+from utils.helper_functions import parse_input, retreive_query_data
 
 st.title("Xerophyta Database Explorer")
 st.divider()
@@ -85,113 +85,7 @@ def main():
             selected_species = st.session_state.species
             input_genes = parse_input(input_genes)
 
-            annotation_data = []
-            matched_input = set()
-            missing_input = set()
-            if st.session_state.gene_input_type == "Gene_ID":
-                annotation_data = database.get_gene_annotation_data(input_genes, "xerophyta_gene_name",selected_species)
-            
-                if annotation_data:
-                    matched_input = {gene.gene_name.lower() for gene in annotation_data}
-                else:
-                    matched_input = set()
-                missing_input = {gene for gene in input_genes if gene.lower() not in matched_input}
-
-            elif st.session_state.gene_input_type == "Arab_loci":
-                annotation_data = database.get_gene_annotation_data(input_genes, "a_thaliana_locus",selected_species)
-            
-                if annotation_data:
-                    matched_input = {gene.arabidopsis_homologues[0].a_thaliana_locus.lower() for gene in annotation_data}
-                
-                else:
-                    matched_input = set()
-                
-                missing_input = {gene for gene in input_genes if gene.lower() not in matched_input}
-
-
-            elif st.session_state.gene_input_type == "Arab_common_name":
-                annotation_data = database.get_gene_annotation_data(input_genes, "a_thaliana_common_name",selected_species)
-
-                if annotation_data:
-                    # Determine which search terms did not match any homologues
-                    for gene in annotation_data:
-                        for homologue in gene.arabidopsis_homologues:
-                            common_name = homologue.a_thaliana_common_name.lower()
-                            for term in input_genes:
-                                if term.lower() in common_name:
-                                    matched_input.add(term)
-
-                missing_input = [term for term in input_genes if term not in matched_input]
-
-            elif st.session_state.gene_input_type == "GO_id":
-                annotation_data = database.get_gene_annotation_data(input_genes, "go_id",selected_species)
-                
-                normalized_inputs = {database.normalize_go_term(term) for term in input_genes}
-
-                if annotation_data:
-                    for gene in annotation_data:
-                        for annotation in gene.annotations:
-                            for go in annotation.go_ids:
-                                normalized_go = database.normalize_go_term(go.go_id)
-                                # Check for partial match: if either string contains the other.
-                                # print(f'normalized_go: {normalized_go}')
-                                for user_term in normalized_inputs:
-                                    if user_term in normalized_go:
-                                        matched_input.add(normalized_go)
-
-                else:
-                    matched_input = set()
-                missing_input = normalized_inputs - matched_input
- 
-            elif st.session_state.gene_input_type == "GO_name":
-                annotation_data = database.get_gene_annotation_data(input_genes, "go_name",selected_species)
-                if annotation_data:
-                    for gene in annotation_data:
-                        for annotation in gene.annotations:
-                            # iterate over GO objects via the go_ids relationship
-                            for go in annotation.go_ids:
-                                # Ensure go.go_name exists and normalize it to lowercase
-                                go_name = go.go_name.lower() if go.go_name else ""
-                                for term in input_genes:
-                                    # Check if the lowercase search term is in the GO name
-                                    if term.lower() in go_name:
-                                        matched_input.add(term)
-                missing_input = [term for term in input_genes if term.lower() not in {m.lower() for m in matched_input}]
-
-            elif st.session_state.gene_input_type == "EC_code":
-                annotation_data = database.get_gene_annotation_data(input_genes, "enzyme_code",selected_species)
-                
-                matched_input = set()
-                normalized_inputs = {term for term in input_genes}
-                if annotation_data:
-                    for gene in annotation_data:
-                        for annotation in gene.annotations:
-                            for enzyme_code in annotation.enzyme_codes:
-                                for user_term in input_genes:
-                                    if user_term in enzyme_code.enzyme_code:
-                                        matched_input.add(enzyme_code.enzyme_code)
-                                
-
-                else:
-                    matched_input = set()
-                missing_input = normalized_inputs - matched_input
-            
-            elif st.session_state.gene_input_type == "EC_name":
-                annotation_data = database.get_gene_annotation_data(input_genes, "enzyme_name",selected_species)
-                if annotation_data:
-                    for gene in annotation_data:
-                        for annotation in gene.annotations:
-                            # iterate over GO objects via the go_ids relationship
-                            for enzyme in annotation.enzyme_codes:
-                                # Ensure go.go_name exists and normalize it to lowercase
-                                enzyme_name = enzyme.enzyme_name.lower() if enzyme.enzyme_name else ""
-                                for term in input_genes:
-                                    # Check if the lowercase search term is in the GO name
-                                    if term.lower() in enzyme_name:
-                                        matched_input.add(term)
-                missing_input = [term for term in input_genes if term.lower() not in {m.lower() for m in matched_input}]
-
-               
+            annotation_data, matched_input, missing_input = retreive_query_data(input_genes, selected_species, st.session_state.gene_input_type)
 
             results= database.flatten_gene_annotation_data(annotation_data)
             df = pd.DataFrame(results)
--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,4 +1,8 @@
 import re 
+import database.db as db
+
+
+
 
 
 def parse_input(input):
@@ -16,3 +20,113 @@ def parse_input(input):
     # Return unique tokens
     return tokens
 
+def retreive_query_data(input_genes,selected_species, gene_input_type):
+    database = db.DB()
+    annotation_data = []
+    matched_input = set()
+    missing_input = set()
+
+    if gene_input_type == "Gene_ID":
+        annotation_data = database.get_gene_annotation_data(input_genes, "xerophyta_gene_name",selected_species)
+    
+        if annotation_data:
+            matched_input = {gene.gene_name.lower() for gene in annotation_data}
+        else:
+            matched_input = set()
+        missing_input = {gene for gene in input_genes if gene.lower() not in matched_input}
+
+    elif gene_input_type == "Arab_loci":
+        annotation_data = database.get_gene_annotation_data(input_genes, "a_thaliana_locus",selected_species)
+    
+        if annotation_data:
+            matched_input = {gene.arabidopsis_homologues[0].a_thaliana_locus.lower() for gene in annotation_data}
+        
+        else:
+            matched_input = set()
+        
+        missing_input = {gene for gene in input_genes if gene.lower() not in matched_input}
+
+
+    elif gene_input_type == "Arab_common_name":
+        annotation_data = database.get_gene_annotation_data(input_genes, "a_thaliana_common_name",selected_species)
+
+        if annotation_data:
+            # Determine which search terms did not match any homologues
+            for gene in annotation_data:
+                for homologue in gene.arabidopsis_homologues:
+                    common_name = homologue.a_thaliana_common_name.lower()
+                    for term in input_genes:
+                        if term.lower() in common_name:
+                            matched_input.add(term)
+
+        missing_input = [term for term in input_genes if term not in matched_input]
+
+    elif gene_input_type == "GO_id":
+        annotation_data = database.get_gene_annotation_data(input_genes, "go_id",selected_species)
+        
+        normalized_inputs = {database.normalize_go_term(term) for term in input_genes}
+
+        if annotation_data:
+            for gene in annotation_data:
+                for annotation in gene.annotations:
+                    for go in annotation.go_ids:
+                        normalized_go = database.normalize_go_term(go.go_id)
+                        # Check for partial match: if either string contains the other.
+                        # print(f'normalized_go: {normalized_go}')
+                        for user_term in normalized_inputs:
+                            if user_term in normalized_go:
+                                matched_input.add(normalized_go)
+
+        else:
+            matched_input = set()
+        missing_input = normalized_inputs - matched_input
+
+    elif gene_input_type == "GO_name":
+        annotation_data = database.get_gene_annotation_data(input_genes, "go_name",selected_species)
+        if annotation_data:
+            for gene in annotation_data:
+                for annotation in gene.annotations:
+                    # iterate over GO objects via the go_ids relationship
+                    for go in annotation.go_ids:
+                        # Ensure go.go_name exists and normalize it to lowercase
+                        go_name = go.go_name.lower() if go.go_name else ""
+                        for term in input_genes:
+                            # Check if the lowercase search term is in the GO name
+                            if term.lower() in go_name:
+                                matched_input.add(term)
+        missing_input = [term for term in input_genes if term.lower() not in {m.lower() for m in matched_input}]
+
+    elif gene_input_type == "EC_code":
+        annotation_data = database.get_gene_annotation_data(input_genes, "enzyme_code",selected_species)
+        
+        matched_input = set()
+        normalized_inputs = {term for term in input_genes}
+        if annotation_data:
+            for gene in annotation_data:
+                for annotation in gene.annotations:
+                    for enzyme_code in annotation.enzyme_codes:
+                        for user_term in input_genes:
+                            if user_term in enzyme_code.enzyme_code:
+                                matched_input.add(enzyme_code.enzyme_code)
+                        
+
+        else:
+            matched_input = set()
+        missing_input = normalized_inputs - matched_input
+    
+    elif gene_input_type == "EC_name":
+        annotation_data = database.get_gene_annotation_data(input_genes, "enzyme_name",selected_species)
+        if annotation_data:
+            for gene in annotation_data:
+                for annotation in gene.annotations:
+                    # iterate over GO objects via the go_ids relationship
+                    for enzyme in annotation.enzyme_codes:
+                        # Ensure go.go_name exists and normalize it to lowercase
+                        enzyme_name = enzyme.enzyme_name.lower() if enzyme.enzyme_name else ""
+                        for term in input_genes:
+                            # Check if the lowercase search term is in the GO name
+                            if term.lower() in enzyme_name:
+                                matched_input.add(term)
+        missing_input = [term for term in input_genes if term.lower() not in {m.lower() for m in matched_input}]
+
+    return annotation_data, matched_input, missing_input
