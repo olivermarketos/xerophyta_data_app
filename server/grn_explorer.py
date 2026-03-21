8a2feb4bce87798d63f2f76a8c1da068c9f8e43e
Oliver Marketos <olivermarketos@gmail.com>
2026-03-21 22:13:03 +0100

    feat: Find which target or TF clusters a gene belongs to
    Only works for X. elegans IDs at the moment

--- server/grn_explorer.py
+++ server/grn_explorer.py
@@ -10,6 +10,7 @@ from sqlalchemy import func
 
 #TODO allow selecting more than one TF group at a time, loop over each group and agg results?
 
+@st.cache_data
 def get_clusters(tf, verbose=False):
 
     if verbose:
@@ -44,6 +45,34 @@ def get_clusters(tf, verbose=False):
         )
     return query.all()
 
+def get_gene_groups(gene):
+    RegulatorGene = aliased(Gene)
+    TargetGene = aliased(Gene)
+    
+    target_clusters = (
+            database.session.query(
+            RegulatoryInteraction.target_cluster
+            )
+            .join(TargetGene, RegulatoryInteraction.target_gene_id == TargetGene.id)
+            .filter(
+                 TargetGene.gene_name == gene
+            ).distinct()
+            ).all()
+    regulatory_clusters =  ( 
+            database.session.query(
+            RegulatoryInteraction.regulatory_cluster,
+            )
+            .join(RegulatorGene, RegulatoryInteraction.regulator_gene_id == RegulatorGene.id)
+            .filter(
+                RegulatorGene.gene_name == gene 
+            ).distinct()
+        ).all()
+    
+    results = {"target_clusters":target_clusters, "regulatory_clusters":regulatory_clusters}
+
+    return results
+
+
 @st.cache_data
 def get_tf_groups():
     result = [r[0] for r in database.session.query(RegulatoryInteraction.regulatory_cluster).distinct()]
@@ -75,12 +104,12 @@ clusters, gene_info = st.tabs(
 if clusters.open:
     with clusters:
         st.markdown("""
-            - Enter ID of a TF group from the above figure (eg C2H2:2) and the Data Explorer will list all the TG clusters predicted to be regulated. 
+            - Enter ID of a TF group from the above figure and the Data Explorer will list all the TG clusters predicted to be regulated. 
             """)
 
 
         TFs = get_tf_groups()
-        verbose = st.checkbox("show genes in clusters")
+        verbose = st.checkbox("Show all genes in clusters (aggregated into lists for readability)")
         selected_tf = st.selectbox("Enter factor",TFs, index=None,label_visibility="hidden", placeholder="Select a transcription factor...")
         clusters = get_clusters(selected_tf, verbose)
         if selected_tf is not None:
@@ -92,4 +121,21 @@ if gene_info.open:
                     - Enter _X. elegans_ ID and retrieve all the TF and Target gene groups 
                     """)
         x_elegans_genes = get_genes_list()
-        selected_tf = st.selectbox("Enter gene ID",x_elegans_genes, index=None,label_visibility="hidden", placeholder="Select an X elegans gene ID...")
+        selected_gene = st.selectbox("Enter gene ID",x_elegans_genes, index=None,label_visibility="hidden", placeholder="Select an X elegans gene ID...")
+        gene_groups = get_gene_groups(selected_gene)
+        if selected_gene is not None:
+            gene_groups = get_gene_groups(selected_gene)
+            
+            # Display regulatory clusters (where gene is a regulator)
+            st.subheader("Regulatory Clusters")
+            if gene_groups['regulatory_clusters']:
+                st.dataframe(gene_groups['regulatory_clusters'])
+            else:
+                st.info(f"No regulatory clusters found where {selected_gene} acts as a regulator.")
+            
+            # Display target clusters (where gene is a target)
+            st.subheader("Target Clusters")
+            if gene_groups['target_clusters']:
+                st.dataframe(gene_groups['target_clusters'])
+            else:
+                st.info(f"No target clusters found where {selected_gene} is a target.")
