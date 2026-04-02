b3a02253790e3a8e4cfa4fe7809e1631fdbc2cbf
Oliver Marketos <olivermarketos@gmail.com>
2026-04-02 11:10:45 +0200

    removed analytics tracking

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 import streamlit as st
-import streamlit.components.v1 as components
-import streamlit_analytics
 
 st.set_page_config(page_title="Data explorer",page_icon=":material/edit:",layout="wide")
 
-# Start tracking analytics
-with streamlit_analytics.track():
     # entry point app
-    home_page = st.Page("server/home.py", title="Home", icon="🏠") #icon=":material/add_circle:")
-    expression_page = st.Page("server/expression_page.py", title="Expression data",icon="📊")
-    #  TODO swap the test_page for the gene_query_page at some point
-    gene_query_page= st.Page("server/gene_query_page.py", title="Gene info",icon="📑")
-    grn_explorer = st.Page("server/grn_explorer.py", title="Gene Regulatory Network", icon="🌐")
+home_page = st.Page("server/home.py", title="Home", icon="🏠") #icon=":material/add_circle:")
+expression_page = st.Page("server/expression_page.py", title="Expression data",icon="📊")
+#  TODO swap the test_page for the gene_query_page at some point
+gene_query_page= st.Page("server/gene_query_page.py", title="Gene info",icon="📑")
+grn_explorer = st.Page("server/grn_explorer.py", title="Gene Regulatory Network", icon="🌐")
 
 
-    pg = st.navigation([home_page,expression_page,gene_query_page, grn_explorer])
+pg = st.navigation([home_page,expression_page,gene_query_page, grn_explorer])
 
-    pg.run()
+pg.run()
 
